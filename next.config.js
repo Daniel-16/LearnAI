f06b68eebc0ad8f8a4544c7a9cccc506e3a95b6c
Daniel Toba <danieloloruntoba681@gmail.com>
2024-12-20 12:19:37 +0100

    todo: fix pdf-dist

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    unoptimized: true
   },
-  images: { unoptimized: true },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    
+    // Add rule for PDF worker
+    config.module.rules.push({
+      test: /pdf\.worker\.js$/,
+      type: 'asset/resource'
+    });
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
